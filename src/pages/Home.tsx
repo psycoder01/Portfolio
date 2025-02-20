8ab18b959b8125fa63cdf9851e8ad4099c6c60bf
psycoder01 <aakashchy13@gmail.com>
2025-02-20 23:23:11 +0000

    feat: added information section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,21 @@ import { InputPrompt } from "@/components/derived/Input";
 import { executeCommand } from "@/pages/commandExecutor";
 import { Commands } from "@/bin/types";
 
+const Information = () => (
+    <div className="my-4">
+        <pre>
+            Type <span className="font-bold">help</span> to see list of
+            available commands.
+        </pre>
+        <pre>
+            Type <span className="font-bold">neofetch</span> to display summary.
+        </pre>
+        <pre>
+            Type <span className="font-bold">repo</span> to go to my GitHub.
+        </pre>
+    </div>
+);
+
 export default function Home() {
     const ref = useRef<HTMLInputElement>(null);
     const { command, setCommand, history, setHistory, clearHistory } =
@@ -28,6 +43,7 @@ export default function Home() {
 
     return (
         <div className="p-8 overflow-hidden h-full border-2 rounded border-light-yellow dark:border-dark-yellow">
+            {history.length === 0 && <Information />}
             <div className="overflow-y-auto h-full">
                 {history.map((h) => (
                     <div key={h.id}>{h.output}</div>
