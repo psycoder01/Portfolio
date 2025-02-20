f010b03756d8bbfc0b1a3c5b67b3398fd0f1f76e
psycoder01 <aakashchy13@gmail.com>
2025-02-20 23:39:21 +0000

    feat: added help command and output

--- src/pages/commands/Help.tsx
+++ src/pages/commands/Help.tsx
@@ -1,5 +1,14 @@
+import { Commands } from "@/bin/types";
+
 const Help = () => {
-    return <div>help</div>;
+    return (
+        <div className="py-4">
+            <div>Available commands : </div>
+            {Object.values(Commands).map((cmd) => (
+                <span className="mr-4 font-bold">{cmd}</span>
+            ))}
+        </div>
+    );
 };
 
 export default Help;
