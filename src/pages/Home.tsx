e013f1b11cfc262ca793fbe7c15ea0fb93f877da
psycoder01 <aakashchy13@gmail.com>
2025-02-28 09:51:02 +0000

    feat: up and down arrow key functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 
 import { useHistory } from "@/hooks";
 import { Commands } from "@/bin/types";
@@ -12,6 +12,8 @@ export default function Home() {
     const { command, setCommand, history, setHistory, clearHistory } =
         useHistory([]);
 
+    const [currentIndex, setCurrentIndex] = useState(0);
+
     const handleEnterKey = (command: String) => {
         if (command === Commands.clear) {
             clearHistory();
@@ -23,9 +25,35 @@ export default function Home() {
         setCommand("");
     };
 
+    const handleArrowKey = (key: "up" | "down") => {
+        let index = 0;
+
+        if (key == "up") {
+            if (currentIndex === 0) return;
+
+            index = currentIndex - 1;
+        }
+        if (key == "down") {
+            if (currentIndex === history.length - 1) return;
+
+            index = currentIndex + 1;
+        }
+
+        const currentHistory = history[index];
+
+        setCommand(currentHistory.command);
+        setCurrentIndex(index);
+    };
+
     const handleAction = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter") return handleEnterKey(command);
+        if (e.key === "ArrowUp") return handleArrowKey("up");
+        if (e.key === "ArrowDown") return handleArrowKey("down");
     };
+
+    useEffect(() => {
+        setCurrentIndex(history.length);
+    }, [history.length]);
 
     return (
         <div className="p-8 overflow-x-hidden overflow-y-auto h-full border-2 rounded border-light-yellow dark:border-dark-yellow">
